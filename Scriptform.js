4c722173775b2ea5bfd4be71f2060a69d2f175be
Fethi <54514676+Whavi@users.noreply.github.com>
2022-11-14 16:14:22 +0100

    Create Scriptform.js

---
+++ Scriptform.js
@@ -0,0 +1,5 @@
+function ShowHideDiv() {
+    var chkYes = document.getElementById("dot-1");
+    var Numen = document.getElementById("numen");
+    Numen.style.display = chkYes.checked ? "block" : "none";
+}
