2b7b49525f4bf68b43fbd84c6228e4b1c57798a1
DDESIR09 <115475385+DDESIR09@users.noreply.github.com>
2022-11-28 17:22:52 +0100

    Add files via upload

---
+++ EXO2_validationLogin.php
@@ -0,0 +1,53 @@
+<?php
+require "./connectionBDD.php" ;
+
+// Si il y a une entrée 'soumettre' comme clef dans la variable superglobale $_POST
+// alors on valorise $utilisateur à la valeur correspondante à la clef identifiant
+// si elle existe et à null sinon
+// Pareil pour $password
+// Autrement dit: si l'utilisateur a entré du texte pour le champ de formulaire
+// ce texte va dans la variable utilisateur
+// Et si il a saisi quelque chose dans le champ motDePasse ce texte va dans le
+// variable $password
+
+if (isset($_POST['soumettre'])) {
+    $utilisateur = (isset($_POST['identifiant'])) ? $_POST['identifiant'] :null;
+    $password = (isset($_POST['motDePasse'])) ? $_POST['motDePasse'] :null;
+    $numen = (isset($_POST['ID_NUMEN'])) ? $_POST['ID_NUMEN']:null;
+}
+
+if(!empty($utilisateur)&& !empty($password) && !empty($numen)) {
+
+$hashDuMotDePasse = password_hash($password, PASSWORD_ARGON2ID);
+$sql = "select ID_NUMEN, identifiant, motDePasse from comptes where identifiant= ?" ;
+$connection = getConnection();
+$instructions = $connection->prepare($sql);
+$instructions->bindParam(1, $utilisateur, PDO::PARAM_STR);
+$instructions->execute();
+$resultat = $instructions->fetchAll() ;
+
+print_r($resultat);
+$loginValide = false;
+// Récupération ici du hash du mot de passe en provenance de la BDD situé dans $resultat
+
+// Vérification avec la fonction PHP password_verify
+
+if (password_verify($password, $hashDuMotDePasse)) {
+    echo 'Password is valid!';
+    $loginValide == true;
+    
+} else {
+    echo 'Invalid password.';
+    $loginValide == false;
+    header("Location: formu.html");
+}
+
+// Si vérification valide, on affiche que l'utilisateur est bien loggé
+if ($loginValide == true){
+echo '<script type="text/javascript">alert("Utilisateur bien loggé"); </script>';  
+header("Location: formuinscription.html");
+} else {
+echo ("<script type='text/javascript'>alert('identifiant ou mot de passe invalide'); </script>"); 
+}
+}
+?>
